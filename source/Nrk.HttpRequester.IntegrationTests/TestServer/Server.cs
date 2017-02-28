88d62f1a49df84632478500d7835bc29480fa42e
Stian Haga <hagastian@gmail.com>
2017-02-28 15:20:24 +0100

    Add integration test project with first test

---
+++ source/Nrk.HttpRequester.IntegrationTests/TestServer/Server.cs
@@ -0,0 +1,21 @@
+﻿using Nancy;
+using Owin;
+
+namespace Nrk.HttpRequester.IntegrationTests.TestServer
+{
+    public class ServerModule : NancyModule
+    {
+        public ServerModule()
+        {
+            Get["/"] = _ => "success";
+        }
+    }
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            appBuilder.UseNancy();
+        }
+    }
+}
---
+++ source/Nrk.HttpRequester.IntegrationTests/WebRequesterIntegrationTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin.Hosting;
+using Nrk.HttpRequester.IntegrationTests.TestServer;
+using Shouldly;
+using Xunit;
+
+namespace Nrk.HttpRequester.IntegrationTests
+{
+    public class WebRequesterIntegrationTests : IDisposable
+    {
+        private readonly IDisposable _webApp;
+        private const string Url = "http://localhost:9001";
+        public WebRequesterIntegrationTests()
+        {
+            _webApp = WebApp.Start<Startup>(Url);
+        }
+
+        [Fact]
+        public async Task GetResponseAsync_ShouldGetResponseFromServer()
+        {
+            // Arrange
+            var httpClient = WebRequestHttpClientFactory.Configure(new Uri(Url)).Create();
+            var webRequester = new WebRequester(httpClient);
+            var response = await webRequester.GetResponseAsync("");
+            response.IsSuccessStatusCode.ShouldBeTrue();
+        }
+
+        public void Dispose()
+        {
+            _webApp.Dispose();
+        }
+    }
+}
