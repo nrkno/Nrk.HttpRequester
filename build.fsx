53be05124cc1b4b3043f007dbe880f498f87a6e9
Scott Tasker <scott.tasker@nrk.no>
2019-04-30 12:21:57 +0200

    Fix error FS1161: TABs are not allowed in F# code unless the #indent "off" option is used

--- build.fsx
+++ build.fsx
@@ -65,7 +65,7 @@ Target "CreateNugetPackage" (fun _ ->
       Publish = false
       Dependencies =
         ["Microsoft.AspNet.WebApi.Client", GetPackageVersion "./source/packages/" "Microsoft.AspNet.WebApi.Client",
-		"Newtonsoft.Json", "./source/packages/" "Newtonsoft.Json"]
+        "Newtonsoft.Json", "./source/packages/" "Newtonsoft.Json"]
       Files =
         [(@"Nrk.HttpRequester.dll", Some @"lib\net45", None)
          (@"Nrk.HttpRequester.pdb", Some @"lib\net45", None)]
